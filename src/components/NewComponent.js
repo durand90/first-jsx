70dece551717d792fbbebc4d7bf2d1b2c6b440f1
Fanfan durand <durand90@live.com>
2023-05-08 13:05:41 -0400

    two

--- src/components/NewComponent.js
+++ src/components/NewComponent.js
@@ -3,15 +3,17 @@ import React, { Component } from 'react'
 class NewComponent extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello Dojo!</h1>
-                <label><strong>Things I need to do:</strong></label>
+            <div style={(StyleSheet.author, { textAlign: "left" })}>
+                <h1 style={{color: "black"}}>Hello Dojo!</h1>
+                <label ><strong>Things I need to do:</strong></label>
                 <ul>
                     <li>Learn React</li>
                     <li>Climb Mt. Everest</li>
                     <li>Run a marathon</li>
                     <li>Feed the dogs</li>
                 </ul>
+
+                
             </div>
         );
     }
